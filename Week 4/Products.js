95d15d5df080e7ded1f9a5ae1a5db62924e5cd93
MonicaMolly <124466917+MonicaMolly@users.noreply.github.com>
2024-11-26 15:15:21 +0400

    Add files via upload

---
+++ Week 4/Products.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of irresistible organic goodness for your cat.",
+        "price": 20.00,
+        "image": "images/cat.jpg",
+        "availableInventory": 10,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> time.",
+        "price": 2.99,
+        "image": "images/yarn.jpg",
+        "availableInventory": 7,
+        "rating": 5
+    },
+    {
+        "id": 1003,
+        "title": "Sign",
+        "description": "Learning sign language.",
+        "price": 10.99,
+        "image": "images/signs.webp",
+        "availableInventory": 6,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "title": "Africabian",
+        "description": "For your travel and dream come true.",
+        "price": 200.00,  
+        "image": "images/africabian.jpg",
+        "availableInventory": 4,
+        "rating": 4
+    }
+];
